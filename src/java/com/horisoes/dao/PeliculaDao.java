d3ba47716581a7656298d74ba532c20cab402112
Personal <Personal@192.168.180.145>
2020-03-14 23:07:16 -0500

    Se realiza la parte del top de peliculas/series

--- src/java/com/horisoes/dao/PeliculaDao.java
+++ src/java/com/horisoes/dao/PeliculaDao.java
@@ -30,7 +30,7 @@ public class PeliculaDao extends DAO
 
             st.setString(1, pel.getNombre());
             st.setString(2, pel.getTipo());
-            st.setString(3, pel.getImagen());
+            st.setInt(3, pel.getIdpeli_serie());
             st.setInt(4, pel.getCalificacion());
             st.setInt(5, pel.getDuracion());
 
@@ -55,7 +55,7 @@ public class PeliculaDao extends DAO
         try {
             PreparedStatement st ;
             this.ConectarBd();
-     st = (PreparedStatement) this.getCn().prepareStatement("SELECT idpeli_serie,nombre,tipo,imagen,calificacion,duracion FROM peli_serie");
+     st = (PreparedStatement) this.getCn().prepareStatement("SELECT idpeli_serie,nombre,tipo,imagen,calificacion,duracion FROM peli_serie ");
               
             rs = st.executeQuery();
             listPelicula = new ArrayList<>();
@@ -70,6 +70,9 @@ public class PeliculaDao extends DAO
             pel.setDuracion(rs.getInt("duracion"));
             listPelicula.add(pel);
 
+     
+
+                        
             }
 
         } catch (Exception e) {
@@ -99,6 +102,8 @@ public class PeliculaDao extends DAO
                 pel.setCalificacion(rs.getInt("calificacion"));
                 pel.setDuracion(rs.getInt("duracion"));
             }
+            FacesMessage mesanjess = new FacesMessage ("Registro Eliminado");
+            FacesContext.getCurrentInstance().addMessage(null, mesanjess);
 
         } catch (Exception e) {
             throw e;
@@ -140,13 +145,14 @@ public class PeliculaDao extends DAO
             PreparedStatement st = (PreparedStatement) this.getCn().prepareStatement("UPDATE peli_serie SET nombre = ?, tipo  = ?,imagen  = ?,calificacion  = ?,duracion= ? WHERE idpeli_serie=?  ");
             st.setString(1, pel.getNombre());
             st.setString(2, pel.getTipo());
-            st.setString(3, pel.getImagen());
+            st.setInt(3, pel.getIdpeli_serie());
             st.setInt(4, pel.getCalificacion());
             st.setInt(5, pel.getDuracion());
             st.setInt(6, pel.getIdpeli_serie());
 
             st.executeUpdate();
-
+            FacesMessage mesanjess = new FacesMessage ("Datos Actualizaos");
+            FacesContext.getCurrentInstance().addMessage(null, mesanjess);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(PeliculaDao.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -156,4 +162,39 @@ public class PeliculaDao extends DAO
     }
 
 
+    
+        public List<Pelicula> listarPeliculasTop(boolean upComing) throws Exception {
+
+        List<Pelicula> listPelicula;
+        ResultSet rs;
+        try {
+            PreparedStatement st ;
+            this.ConectarBd();
+        st = (PreparedStatement) this.getCn().prepareStatement("SELECT idpeli_serie,nombre,tipo,imagen,calificacion,duracion FROM peli_serie order by calificacion desc");
+              
+            rs = st.executeQuery();
+            listPelicula = new ArrayList<>();
+            while (rs.next()) {
+            Pelicula pel = new Pelicula();                
+               
+            pel.setIdpeli_serie(rs.getInt("idpeli_serie"));
+            pel.setNombre(rs.getString("nombre"));
+            pel.setTipo(rs.getString("tipo"));
+            pel.setImagen(rs.getString("imagen"));
+            pel.setCalificacion(rs.getInt("calificacion"));
+            pel.setDuracion(rs.getInt("duracion"));
+            listPelicula.add(pel);
+            
+            }
+          FacesMessage mesanjess = new FacesMessage ("Upcoming...Peliculas por mejor Calificacion");
+            FacesContext.getCurrentInstance().addMessage(null, mesanjess);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            desConectarBD();
+        }
+        return listPelicula;
+    }
+    
+    
 }
